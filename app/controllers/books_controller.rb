331aa19c4791d4444ca13d9ae0137ae54c9a6532
EC2 Default User <ec2-user@ip-172-31-88-249.ec2.internal>
2022-03-04 15:36:50 +0000

    [Finish]2-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,51 +1,69 @@
 class BooksController < ApplicationController
+ 
+ before_action :baria_user, only: [:destroy, :update]
   
-  def index
+ def index
+   @books = Book.all
+   @user = User.find(current_user.id)
+   @book = Book.new
+ end
+ 
+ def create
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
+   @user =@book.user
+ if @book.save
+   flash[:notice] = 'You have created book successfully.'
+   redirect_to book_path(@book.id)
+ else
+   @books = Book.all
+   render:index
+ end
+ end
+ 
+ def show
+   @book = Book.find(params[:id])
+   @user = @book.user
+   @newbook =Book.new
+ end
+ 
+ def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
     @books = Book.all
-    @book = Book.new
-  end
-  
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :index
-    end
-  end 
+    redirect_to books_path
+ end
 
-  def show
-    @book = Book.find(params[:id])
-    
-  end
+ def edit
+  @book = Book.find(params[:id])
+ if @book.user_id == current_user.id
+    render:edit
+ else
+    redirect_to books_path
+ end
+ end
 
-  def edit
-    @book = Book.find(params[:id])
-  end
-  
-  def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      flash[:notice] = 'Book was successfully updated.'
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :show
-    end
-  end
-  
-  def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
-  end
-  
-  private
-  
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end 
+ def update
+   @book =Book.find(params[:id])
+ if @book.update(book_params)
+   flash[:notice] = 'You have updated book successfully.'
+   redirect_to book_path(@book.id)
+ else
+   render:edit
+ end
+ end
+
+ private
+ 
+ def user_params
+   params.require(:user).permit(:name, :profile_image, :introduction)
+ end
+ def book_params
+   params.require(:book).permit(:title, :body)
+ end
+ def baria_user
+   unless Book.find(params[:id]).user.id.to_i == current_user.id
+        redirect_to books_path
+   end
+ end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,57 @@
 class UsersController < ApplicationController
-  def index
-    @book = Book.new
-    @users = User.page(params[:page]).reverse_order
-  end
-
+  
   def show
     @user = User.find(params[:id])
+    @books = Book.where(user_id: @user.id)
     @book = Book.new
-    @books = @user.books.page(params[:page]).reverse_order
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = 'You have created book successfully.'
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render:index
+    end
   end
 
+  def index
+    @users = User.all
+    @books = Book.where(user_id: current_user.id)
+    @book = Book.new
+  end
+  
   def edit
     @user = User.find(params[:id])
+    if @user.id == current_user.id
+      render:edit
+    else
+      @books = Book.where(user_id: @user.id)
+      @book = Book.new
+      redirect_to user_path(current_user.id)
+    end
   end
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)  
+  if @user.update(user_params)
+    flash[:notice] = 'You have updated user successfully.'
+    redirect_to user_path(@user.id)
+  else
+    render:edit
+  end
+  end
+
+ private
+ 
+  def user_params
+  params.require(:user).permit(:name, :introduction, :profile_image)
   end
   
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :profile_image, :introduction)
+  def book_params
+  params.requrire(:book).permit(:title, :body)
   end
-
-
 end
